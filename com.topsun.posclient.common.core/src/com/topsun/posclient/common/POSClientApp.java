5e2a4e12af6bf78230099463e78f62a0b1b20938
tandong8888 <935760092@qq.com>
2013-09-06 11:26:36 -0700

    添加三个方法getData和getSysConfig

--- com.topsun.posclient.common.core/src/com/topsun/posclient/common/POSClientApp.java
+++ com.topsun.posclient.common.core/src/com/topsun/posclient/common/POSClientApp.java
@@ -1,43 +1,77 @@
 package com.topsun.posclient.common;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.topsun.posclient.datamodel.SettingData;
 import com.topsun.posclient.datamodel.User;
 
 /**
  * @author Dong
- *
+ * 
  */
 public class POSClientApp {
-	
+
 	/**
 	 * 
 	 */
 	static POSClientApp posClientApp;
-	
+
 	/**
 	 * 
 	 */
 	private Map<String, Object> contextData = new HashMap<String, Object>();
-	
-	private POSClientApp(){}
-	
-	public static POSClientApp get(){
-		if(null == posClientApp){
+
+	private POSClientApp() {
+	}
+
+	public static POSClientApp get() {
+		if (null == posClientApp) {
 			posClientApp = new POSClientApp();
 			return posClientApp;
 		}
 		return posClientApp;
 	}
-	
-	public void saveLoginUser(User loginUser){
+
+	public void saveLoginUser(User loginUser) {
 		contextData.put(AppConstants.LOGIN_USER, loginUser);
 	}
-	
-	public User getLoginUser(){
-		return (User)contextData.get(AppConstants.LOGIN_USER);
+
+	public User getLoginUser() {
+		return (User) contextData.get(AppConstants.LOGIN_USER);
 	}
 	
+	public void setData(String key ,Object value){
+		if(key == null){
+			return;
+		}
+		contextData.put(key,value);
+	}
+
+	public Object getData(String key){
+		if(key == null){
+			return null;
+		}
+		return contextData.get(key);
+	}
+
+	public SettingData getSysConfig() {
+		String filepath = ProjectUtil.getRuntimeClassPath()
+				+ AppConstants.SEETING_FILE;
+		SettingData settingData = new SettingData();
+		try {
+			String serverIP = ProjectUtil.readValue(filepath, "serverIP");
+			String serverPort = ProjectUtil.readValue(filepath, "serverPort");
+			String reconnectionTime = ProjectUtil.readValue(filepath,
+					"reconnectionTime");
+			settingData.setIp(serverIP);
+			settingData.setPort(serverPort);
+			settingData.setReconnectionTime(reconnectionTime);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return settingData;
+	}
 
 }
